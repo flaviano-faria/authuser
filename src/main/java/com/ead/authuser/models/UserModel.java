bcdb9c6adb39a9b51b7ccc9ad2e2ec0825476edf
flaviano-faria <flsilv89@yahoo.com.br>
2025-06-04 21:10:37 -0300

    Updating UserModel

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -2,12 +2,16 @@ package com.ead.authuser.models;
 
 import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_USERS")
 public class UserModel implements Serializable {
@@ -16,18 +20,39 @@ public class UserModel implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
 
-    @Column(unique = true, nullable = false, length = 50)
+    @Column(nullable = false, length = 20)
     private String username;
 
-
+    @JsonIgnore
+    @Column(unique = true, nullable = false, length = 50)
     private String password;
+
+    @Column(unique = true, nullable = false, length = 50)
     private String email;
+
+    @Column(nullable = false, length = 150)
     private String fullName;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    @Column(length = 20)
     private String phoneNumber;
+
+    @Column(length = 255)
     private String imageUrl;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
     private LocalDateTime creationDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDate;
 
     public UUID getUserId() {
