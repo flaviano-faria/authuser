22cd0872f096b6f016aec412e5a8b79a50fce197
flaviano-faria <flsilv89@yahoo.com.br>
2025-06-27 23:27:45 -0300

    Updating GlobalExceptionHandler

--- src/main/java/com/ead/authuser/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ead/authuser/exceptions/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package com.ead.authuser.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,5 +26,21 @@ public class GlobalExceptionHandler {
         return  ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorRecordResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+           String fieldName  = ((FieldError) error).getField();
+           String errorMessage = error.getDefaultMessage();
+           errors.put(fieldName, errorMessage);
+        });
+
+        var  errorResponse = new ErrorRecordResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Error: Validation failed", errors);
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 
 }
