3d5028b59cf1c7be740e169621b10065c68bce57
flaviano-faria <flsilv89@yahoo.com.br>
2025-08-08 22:15:45 -0300

    Use EnableSpringDataWebSupport

--- src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -4,12 +4,14 @@ import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ResolverConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
