481ea1c9f02e02edbeb38c329c9b2d0434d63a38
flaviano-faria <flsilv89@yahoo.com.br>
2025-11-27 22:11:51 -0300

    Configuring spring security into authuser

---
+++ src/main/java/com/ead/authuser/configs/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.ead.authuser.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+}
