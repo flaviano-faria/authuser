4de83c7ac7e0e6e7d13610ef12e475dc18a3ca65
flaviano-faria <flsilv89@yahoo.com.br>
2025-12-10 21:27:21 -0300

    Updating Controller

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -1,13 +1,21 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.configs.security.JwtProvider;
+import com.ead.authuser.dtos.JwtRecordDto;
+import com.ead.authuser.dtos.LoginRecordDto;
 import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.services.UserService;
 import com.ead.authuser.validations.UserValidator;
 import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +24,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+    private final JwtProvider jwtProvider;
+    private final AuthenticationManager authenticationManager;
     Logger logger = LogManager.getLogger(AuthenticationController.class);
 
     final UserService userService;
     final UserValidator userValidator;
 
-    public AuthenticationController(UserService userService, UserValidator userValidator) {
+    public AuthenticationController(UserService userService, UserValidator userValidator, JwtProvider jwtProvider, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.userValidator = userValidator;
+        this.jwtProvider = jwtProvider;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtRecordDto> authenticateUser(@RequestBody @Valid LoginRecordDto loginRecordDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRecordDto.username(), loginRecordDto.password()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new JwtRecordDto(jwtProvider.generateJwt(authentication)));
     }
 
     @PostMapping("/signup")
